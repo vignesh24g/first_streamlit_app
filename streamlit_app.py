14fae7f8380772208c2f8884a0b87324c35f0fe6
vignesh24g <vignesh24.gv@gmail.com>
2023-09-18 16:20:36 +0530

    Update streamlit_app.py
    display request response in a df-table

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,11 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#streamlit.text(fruityvice_response.json())
+
+# normalize json 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display it in dataframe i.e table format
+streamlit.dataframe(fruityvice_normalized)
 
